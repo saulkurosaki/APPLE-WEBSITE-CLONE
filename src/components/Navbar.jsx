d32e4e4cbf320312b7c9219bd15709eece30f692
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-27 09:20:09 -0600

    feat(Navbar): implement the Apple logo in the top left corner

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,13 @@
+import { appleImg } from "../utils";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header>
+      <nav>
+        <img src={appleImg} alt="Apple logo" width={14} height={18} />
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
