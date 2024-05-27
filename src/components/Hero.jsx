da878e7c2e51738ff43adc5e7a7a9f18803b2dd6
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-27 10:24:01 -0600

    feat(Hero): implement the animated title with GSAP

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,20 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const Hero = () => {
+  useGSAP(() => {
+    gsap.to("#hero", {
+      opacity: 1,
+      delay: 1.5,
+    });
+  }, []);
+
   return (
     <section className="w-full nav-height bg-black relative">
       <div className="h-5/6 w-full flex-center flex-col">
-        <p className="hero-title">Iphone 15 Pro</p>
+        <p id="hero" className="hero-title">
+          Iphone 15 Pro
+        </p>
       </div>
     </section>
   );
