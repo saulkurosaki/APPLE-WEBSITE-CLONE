61b40e122b348335830695ee035f2527bfb69ae1
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-27 09:17:54 -0600

    feat(App): implement the Navbar, Hero and Highlights components within the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+
 const App = () => {
-  return <main className="bg-black">Navbar Hero Highlights</main>;
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
+  );
 };
 
 export default App;
