ca448c6459aa9000ddea0e33f479d7eb44dc0e10
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-27 09:12:51 -0600

    feat(Navbar): implement the Navbar component initial layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <main className="bg-black">Navbar Hero Highlights</main>;
 };
 
 export default App;
