6258f5f543bee883380c308c5b2b74c77ee546d7
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-27 10:36:56 -0600

    feat(Hero): implement the hero section video as well as the handleVideoSrc function to resize the video depending on the window inner width

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,31 @@
+import { useState } from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 
+import { heroVideo, smallHeroVideo } from "../utils";
+import { useEffect } from "react";
+
 const Hero = () => {
+  const [videoSrc, setVideoSrc] = useState(
+    window.innerWidth < 760 ? smallHeroVideo : heroVideo
+  );
+
+  const handleVideoScr = () => {
+    if (window.innerWidth < 760) {
+      setVideoSrc(smallHeroVideo);
+    } else {
+      setVideoSrc(heroVideo);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleVideoScr);
+
+    return () => {
+      window.removeEventListener("resize", handleVideoScr);
+    };
+  }, []);
+
   useGSAP(() => {
     gsap.to("#hero", {
       opacity: 1,
@@ -15,6 +39,18 @@ const Hero = () => {
         <p id="hero" className="hero-title">
           Iphone 15 Pro
         </p>
+
+        <div className="md:w-10/12 w-9/12">
+          <video
+            autoPlay
+            muted
+            playsInline={true}
+            key={videoSrc}
+            className="pointer-events-none"
+          >
+            <source src={videoSrc} type="video/mp4" />
+          </video>
+        </div>
       </div>
     </section>
   );
