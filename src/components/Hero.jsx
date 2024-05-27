d0f0b941c507e02e3385d3a2b6d1af066b821130
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-27 10:20:52 -0600

    install all the gsap library dependecies

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,11 @@
 const Hero = () => {
-  return <section className="w-full nav-height bg-black relative"></section>;
+  return (
+    <section className="w-full nav-height bg-black relative">
+      <div className="h-5/6 w-full flex-center flex-col">
+        <p className="hero-title">Iphone 15 Pro</p>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
