c5653d57fdf5ee5ee51df56e1d376773a7cf8328
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-27 09:34:49 -0600

    feat(Navbar): implement the complete Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,26 @@
-import { appleImg } from "../utils";
+import { appleImg, bagImg, searchImg } from "../utils";
+import { navLists } from "../constants";
 
 const Navbar = () => {
   return (
-    <header>
-      <nav>
+    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
+      <nav className="flex w-full screen-max-width">
         <img src={appleImg} alt="Apple logo" width={14} height={18} />
 
-        <div>
-          {["Phones", "Macbooks", "Tablets"].map((item, i) => (
-            <div key={i}>{item}</div>
+        <div className="flex flex-1 justify-center max-sm:hidden">
+          {navLists.map((item, i) => (
+            <div
+              key={i}
+              className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all"
+            >
+              {item}
+            </div>
           ))}
+        </div>
+
+        <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
+          <img src={searchImg} alt="search" width={18} height={18} />
+          <img src={bagImg} alt="bag" width={18} height={18} />
         </div>
       </nav>
     </header>
