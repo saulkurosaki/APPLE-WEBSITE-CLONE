0ea5057c931a30adb23dc148327c2fcf88c20c5a
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-27 21:57:45 -0600

    feat(Highlights): implement the section watch the film side link

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,5 +1,6 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { watchImg } from "../utils";
 
 const Highlights = () => {
   useGSAP(() => {
@@ -15,10 +16,17 @@ const Highlights = () => {
       className="w-screen overflow-hidden h-full common-padding bg-zinc"
     >
       <div className="screen-max-width">
-        <div>
+        <div className="mb-12 w-full items-end justify-between">
           <h1 id="title" className="section-heading">
             Get the highlights.
           </h1>
+
+          <div className="flex flex-wrap items-end gap-5">
+            <p className="link">
+              Watch the film
+              <img src={watchImg} alt="watch" className="ml-2" />
+            </p>
+          </div>
         </div>
       </div>
     </section>
