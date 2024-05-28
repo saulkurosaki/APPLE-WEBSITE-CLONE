bde45992ffc5645d987ddf3ac454181869d866a0
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-27 21:51:51 -0600

    feat(Highlights): implement the Highlights component container layout

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,5 +1,10 @@
 const Highlights = () => {
-  return <div>Highlights</div>;
+  return (
+    <section
+      id="highlights"
+      className="w-screen overflow-hidden h-full common-padding bg-zinc"
+    ></section>
+  );
 };
 
 export default Highlights;
