be4d59ff00aeca7a670ade502a8308683b653317
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-27 23:07:53 -0600

    feat(VideoCarousel): implement the video card tracker container layout

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -19,28 +19,28 @@ const VideoCarousel = () => {
 
   const { isEnd, startPlay, videoId, isLastVideo, isPlaying } = video;
 
-  useEffect(() => {
-    if (loadedData.length > 3) {
-      if (!isPlaying) {
-        videoRef.current[videoId].pause();
-      } else {
-        isPlaying && videoRef.current[videoId].play();
-      }
-    }
-  }, [startPlay, videoId, isPlaying, loadedData]);
+  //   useEffect(() => {
+  //     if (loadedData.length > 3) {
+  //       if (!isPlaying) {
+  //         videoRef.current[videoId].pause();
+  //       } else {
+  //         isPlaying && videoRef.current[videoId].play();
+  //       }
+  //     }
+  //   }, [startPlay, videoId, isPlaying, loadedData]);
 
-  useEffect(() => {
-    const currentProgress = 0;
-    let span = videoSpanRef.current();
+  //   useEffect(() => {
+  //     const currentProgress = 0;
+  //     let span = videoSpanRef.current();
 
-    if (span[videoId]) {
-      // animate the progress of the video
-      let anim = gsap.to(span[videoId], {
-        onUpdate: () => {},
-        onComplete: () => {},
-      });
-    }
-  }, [videoId, startPlay]);
+  //     if (span[videoId]) {
+  //       // animate the progress of the video
+  //       let anim = gsap.to(span[videoId], {
+  //         onUpdate: () => {},
+  //         onComplete: () => {},
+  //       });
+  //     }
+  //   }, [videoId, startPlay]);
 
   return (
     <>
@@ -76,6 +76,23 @@ const VideoCarousel = () => {
             </div>
           </div>
         ))}
+      </div>
+
+      <div className="relative flex-center mt-10">
+        <div className="flex-center py-5 px-7 bg-gray-300 backdrop-blur rounded-full">
+          {/* {videoRef.current.map((_, i) => (
+            <span
+              key={i}
+              ref={(el) => (videoDivRef.current[i] = el)}
+              className="mx-2 w-3 h-3 bg-gray-200 rounded-full relative cursor-pointer"
+            >
+              <span
+                className="absolute h-full w-full rounded-full"
+                ref={(el) => (videoSpanRef.current[i] = el)}
+              />
+            </span>
+          ))} */}
+        </div>
       </div>
     </>
   );
