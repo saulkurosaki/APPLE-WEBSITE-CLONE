3f0c9cf528ce0541bc10a0c5511a645be8b748f9
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-27 22:02:13 -0600

    feat(Highlights): implement the section watch the event side link along as its GSAP animation

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,6 +1,6 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import { watchImg } from "../utils";
+import { rightImg, watchImg } from "../utils";
 
 const Highlights = () => {
   useGSAP(() => {
@@ -12,6 +12,8 @@ const Highlights = () => {
     gsap.to(".link", {
       opacity: 1,
       y: 0,
+      duration: 1,
+      stagger: 0.25,
     });
   }, []);
 
@@ -21,7 +23,7 @@ const Highlights = () => {
       className="w-screen overflow-hidden h-full common-padding bg-zinc"
     >
       <div className="screen-max-width">
-        <div className="mb-12 w-full items-end justify-between">
+        <div className="mb-12 w-full md:flex items-end justify-between">
           <h1 id="title" className="section-heading">
             Get the highlights.
           </h1>
@@ -30,6 +32,10 @@ const Highlights = () => {
             <p className="link">
               Watch the film
               <img src={watchImg} alt="watch" className="ml-2" />
+            </p>
+            <p className="link">
+              Watch the event
+              <img src={rightImg} alt="right" className="ml-2" />
             </p>
           </div>
         </div>
