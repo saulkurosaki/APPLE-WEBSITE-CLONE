ef3b8510cf5b55291543f538bd613f79ff3ca83a
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-29 09:59:52 -0600

    feat(Model): implement the size and model states

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,8 +1,17 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import ModelView from "./ModelView";
+import { useState } from "react";
+import { yellowImg } from "../utils";
 
 const Model = () => {
+  const [size, setSize] = useState("small");
+  const [model, setModel] = useState({
+    title: "iPhone 15 Pro in Natural Titanium",
+    color: ["#8F8A81", "#FFE7B9", "#6F6C64"],
+    img: yellowImg,
+  });
+
   useGSAP(() => {
     gsap.to("#heading", {
       y: 0,
