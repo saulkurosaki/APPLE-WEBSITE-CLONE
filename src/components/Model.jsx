d0535871a4eb843d1d4fa5f873c658b8db3b977a
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-29 09:54:55 -0600

    feat(Model): implement the model view section container layout

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -15,6 +15,10 @@ const Model = () => {
         <h1 id="heading" className="section-heading">
           Take a closer look.
         </h1>
+
+        <div className="flex flex-col items-center mt-5">
+          <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative"></div>
+        </div>
       </div>
     </section>
   );
