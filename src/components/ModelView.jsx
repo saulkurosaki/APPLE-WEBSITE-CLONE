0e0fc0ee2390d14895356fb1874965d8235c30ed
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-29 15:09:09 -0600

    feat(Loader): implement the complete Loader component

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -9,6 +9,7 @@ import * as THREE from "three";
 
 import Lights from "./Lights";
 import IPhone from "./IPhone";
+import Loader from "./Loader";
 
 const ModelView = ({
   index,
@@ -47,13 +48,7 @@ const ModelView = ({
         name={`${index === 1 ? "small" : "large"}`}
         position={[0, 0, 0]}
       >
-        <Suspense
-          fallback={
-            <Html>
-              <div>Loading</div>
-            </Html>
-          }
-        >
+        <Suspense fallback={<Loader />}>
           <IPhone
             scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
             item={item}
