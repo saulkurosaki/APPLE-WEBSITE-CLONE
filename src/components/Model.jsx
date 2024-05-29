5a231a4a893385680b259a19fc4f57866135e799
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-29 10:34:12 -0600

    feat(Model): implement the complete model color select bar

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -7,6 +7,7 @@ import ModelView from "./ModelView";
 import { yellowImg } from "../utils";
 import { Canvas } from "@react-three/fiber";
 import { View } from "@react-three/drei";
+import { models } from "../constants";
 
 const Model = () => {
   const [size, setSize] = useState("small");
@@ -84,7 +85,16 @@ const Model = () => {
             <p className="text-sm font-light text-center mb-5">{model.title}</p>
 
             <div className="flex-center">
-              <ul className="color-container"></ul>
+              <ul className="color-container">
+                {models.map((item, i) => (
+                  <li
+                    key={i}
+                    className="w-6 h-6 rounded-full mx-2 cursor-pointer"
+                    style={{ backgroundColor: item.color[0] }}
+                    onClick={() => setModel(item)}
+                  />
+                ))}
+              </ul>
             </div>
           </div>
         </div>
