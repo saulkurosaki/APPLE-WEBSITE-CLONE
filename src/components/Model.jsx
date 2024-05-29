8d94f47c66f052779ba3e9a7ccfafd80b16bc2e4
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-29 09:50:43 -0600

    feat(Model): implement the model section title

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,7 +1,11 @@
 const Model = () => {
   return (
     <section className="common-padding">
-      <div className="screen-max-width"></div>
+      <div className="screen-max-width">
+        <h1 id="heading" className="section-heading">
+          Take a closer look.
+        </h1>
+      </div>
     </section>
   );
 };
