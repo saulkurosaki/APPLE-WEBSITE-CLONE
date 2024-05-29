86a3805901371e78b9c1e428e97026a52e1ed6fe
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-29 15:26:10 -0600

    feat(utils/animations.js): implement the complete animateWithGsapTimeline function

--- src/utils/animations.js
+++ src/utils/animations.js
@@ -1,1 +1,32 @@
-export const animateWithGsapTimeline = () => {};
+export const animateWithGsapTimeline = (
+  timeline,
+  rotationRef,
+  rotationState,
+  firstTarget,
+  secondTarget,
+  animationProps
+) => {
+  timeline.to(rotationRef.current.rotation, {
+    y: rotationState,
+    duration: 1,
+    ease: "power2.inOut",
+  });
+
+  timeline.to(
+    firstTarget,
+    {
+      ...animationProps,
+      ease: "power2.inOut",
+    },
+    "<"
+  );
+
+  timeline.to(
+    secondTarget,
+    {
+      ...animationProps,
+      ease: "power2.inOut",
+    },
+    "<"
+  );
+};
