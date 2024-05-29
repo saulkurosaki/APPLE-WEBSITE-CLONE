368201d0c38c54039dc2f6f25c1b36e3f9fdc624
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-29 10:40:25 -0600

    feat(Model): implement the complete model size select bar

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -7,7 +7,7 @@ import ModelView from "./ModelView";
 import { yellowImg } from "../utils";
 import { Canvas } from "@react-three/fiber";
 import { View } from "@react-three/drei";
-import { models } from "../constants";
+import { models, sizes } from "../constants";
 
 const Model = () => {
   const [size, setSize] = useState("small");
@@ -95,6 +95,22 @@ const Model = () => {
                   />
                 ))}
               </ul>
+
+              <button className="size-btn-container">
+                {sizes.map(({ label, value }) => (
+                  <span
+                    key={label}
+                    className="size-btn"
+                    style={{
+                      backgroundColor: size === value ? "white" : "transparent",
+                      color: size === value ? "black" : "white",
+                    }}
+                    onClick={() => setSize(value)}
+                  >
+                    {label}
+                  </span>
+                ))}
+              </button>
             </div>
           </div>
         </div>
