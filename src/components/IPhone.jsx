7238dc3bec451187a61d13887edba949c3954d9a
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-29 15:02:25 -0600

    feat(IPhone): implement the swap between textures feature

--- src/components/IPhone.jsx
+++ src/components/IPhone.jsx
@@ -6,11 +6,31 @@ Source: https://sketchfab.com/3d-models/apple-iphone-15-pro-max-black-df17520841
 Title: Apple iPhone 15 Pro Max Black
 */
 
-import React, { useRef } from "react";
-import { useGLTF } from "@react-three/drei";
+import React, { useEffect, useRef } from "react";
+import { useGLTF, useTexture } from "@react-three/drei";
+import * as THREE from "three";
 
 function Model(props) {
   const { nodes, materials } = useGLTF("/models/scene.glb");
+
+  const texture = useTexture(props.item.img);
+
+  useEffect(() => {
+    Object.entries(materials).map((material) => {
+      // these are the material names that can't be changed color
+      if (
+        material[0] !== "zFdeDaGNRwzccye" &&
+        material[0] !== "ujsvqBWRMnqdwPx" &&
+        material[0] !== "hUlRcbieVuIiOXG" &&
+        material[0] !== "jlzuBkUzuJqgiAK" &&
+        material[0] !== "xNrofRCqOXXHVZt"
+      ) {
+        material[1].color = new THREE.Color(props.item.color[0]);
+      }
+      material[1].needsUpdate = true;
+    });
+  }, [materials, props.item]);
+
   return (
     <group {...props} dispose={null}>
       <mesh
