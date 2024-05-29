d3f4a8101ba1ccd6a56599131bfe705e9cafae27
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-29 13:43:28 -0600

    feat(ModelView): implement the threeJs View component container layout

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,5 +1,23 @@
-const ModelView = () => {
-  return <div>ModelView</div>;
+import { View } from "@react-three/drei";
+
+const ModelView = ({
+  index,
+  groupref,
+  gsapType,
+  controlRef,
+  setRotationSize,
+  size,
+  item,
+}) => {
+  return (
+    <View
+      index={index}
+      id={gsapType}
+      className={`border-2 border-red-500 w-full h-full ${
+        index === 2 ? "right-[-100%]" : ""
+      }`}
+    ></View>
+  );
 };
 
 export default ModelView;
