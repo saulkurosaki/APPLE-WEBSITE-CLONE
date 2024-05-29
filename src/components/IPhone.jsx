433eff352504299eb05b7e660d1c540ffbcec56f
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-29 13:57:37 -0600

    feat(IPhone): implement the IPhone component initial layut

---
+++ src/components/IPhone.jsx
@@ -0,0 +1,5 @@
+const IPhone = () => {
+  return <div>IPhone</div>;
+};
+
+export default IPhone;
--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,6 +1,7 @@
 import { PerspectiveCamera, View } from "@react-three/drei";
 
 import Lights from "./Lights";
+import { Suspense } from "react";
 
 const ModelView = ({
   index,
@@ -25,6 +26,8 @@ const ModelView = ({
       <PerspectiveCamera makeDefault position={[0, 0, 4]} />
 
       <Lights />
+
+      <Suspense fallback={<div>Loading</div>}></Suspense>
     </View>
   );
 };
