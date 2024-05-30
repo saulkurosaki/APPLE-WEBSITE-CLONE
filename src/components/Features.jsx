95146211ff2b65b70825b4bce1a382f332442dea
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-30 14:02:37 -0600

    feat(Features): implement the component title

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -2,7 +2,11 @@ const Features = () => {
   return (
     <section className="h-full common-padding bg-zinc relative overflow-hidden">
       <div className="screen-max-width">
-        <div className="mb-12 w-full"></div>
+        <div className="mb-12 w-full">
+          <h1 id="features_title" className="section-heading">
+            Explore the full story.
+          </h1>
+        </div>
       </div>
     </section>
   );
