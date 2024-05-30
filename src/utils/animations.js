ea999c6ddce89bb9bf37660e3999cd3716517cb3
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-30 14:08:27 -0600

    feat(utils/animations.js): implement the animateWithGsap utility function

--- src/utils/animations.js
+++ src/utils/animations.js
@@ -1,3 +1,17 @@
+import gsap from "gsap";
+
+export const animateWithGsap = (target, animationProps, scrollProps) => {
+  gsap.to(target, {
+    ...animationProps,
+    scrollTrigger: {
+      trigger: target,
+      toggleActions: "restart reverse restart reverse",
+      start: "top 85%",
+      ...scrollProps,
+    },
+  });
+};
+
 export const animateWithGsapTimeline = (
   timeline,
   rotationRef,
