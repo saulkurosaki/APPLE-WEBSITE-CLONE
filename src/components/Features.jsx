c61f87e6728401093a02769ad3a9d6c20a297a01
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-30 14:01:39 -0600

    feat(Features): implement the component container layout

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,5 +1,11 @@
 const Features = () => {
-  return <div>Features</div>;
+  return (
+    <section className="h-full common-padding bg-zinc relative overflow-hidden">
+      <div className="screen-max-width">
+        <div className="mb-12 w-full"></div>
+      </div>
+    </section>
+  );
 };
 
 export default Features;
