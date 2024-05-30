4ae13439c27d99a4e5895029c6c302e535efe1a4
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-30 14:42:51 -0600

    feat(Features): implement the section first text along as its GSAP animations

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -20,6 +20,12 @@ const Features = () => {
         scrub: 5.5,
       }
     );
+    animateWithGsap(".g_text", {
+      y: 0,
+      opacity: 1,
+      ease: "power2.inOut",
+      duration: 1,
+    });
   }, []);
 
   return (
@@ -71,6 +77,31 @@ const Features = () => {
                     className="feature-video g_grow"
                   />
                 </div>
+              </div>
+
+              <div className="feature-text-container">
+                <div className="flex-1 flex-center">
+                  <p className="feature-text g_text">
+                    iPhone 15 Pro is{" "}
+                    <span className="text-white">
+                      the first iPhone to feature an aerospace-grade titanium
+                      design
+                    </span>
+                    , using the same alloy that spacecrafts use for missions to
+                    Mars.
+                  </p>
+                </div>
+
+                {/* <div className="flex-1 flex-center">
+                  <p className="feature-text g_text">
+                    Titanium has one of the best strength-to-weight ratios of
+                    any metal, making these our{" "}
+                    <span className="text-white">
+                      lightest Pro models ever.
+                    </span>{" "}
+                    You'll notice the difference the moment you pick one up.
+                  </p>
+                </div> */}
               </div>
             </div>
           </div>
