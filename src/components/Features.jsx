1258c66842b285097f322deb0fc08a4a64c55492
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-30 14:25:36 -0600

    feat(Features): implement the component video

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,7 +1,12 @@
+import { useRef } from "react";
 import { useGSAP } from "@gsap/react";
 import { animateWithGsap } from "../utils/animations";
 
+import { exploreVideo } from "../utils";
+
 const Features = () => {
+  const videoRef = useRef();
+
   useGSAP(() => {
     animateWithGsap("#features_title", { y: 0, opacity: 1 });
   }, []);
@@ -21,6 +26,22 @@ const Features = () => {
             <h2 className="text-5xl lg:text-7xl font-semibold">
               Forged in titanium.
             </h2>
+          </div>
+
+          <div className="flex-center flex-col sm:px-10">
+            <div className="relative h-[50vh] w-full flex items-center">
+              <video
+                playsInline
+                id="exploreVideo"
+                className="w-full h-full object-cover object-center"
+                preload="none"
+                muted
+                autoPlay
+                ref={videoRef}
+              >
+                <source src={exploreVideo} type="video/mp4" />
+              </video>
+            </div>
           </div>
         </div>
       </div>
