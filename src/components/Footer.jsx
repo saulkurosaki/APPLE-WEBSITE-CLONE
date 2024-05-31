46279128487aa937a75a15c357263b150d89bdef
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-30 20:20:34 -0600

    feat(Footer): implement the component container layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,9 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="py-5 sm:px-10 px-5">
+      <div className="screen-max-width"></div>
+    </footer>
+  );
 };
 
 export default Footer;
