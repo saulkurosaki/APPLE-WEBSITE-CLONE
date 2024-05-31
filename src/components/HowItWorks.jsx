1b3f325f15eede92131d1988699c1fe3f4234dd0
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-30 19:18:53 -0600

    feat(HowItWorks): implement the section apple chip image

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -1,7 +1,13 @@
+import { chipImg } from "../utils";
+
 const HowItWorks = () => {
   return (
     <section className="common-padding">
-      <div className="screen-max-width"></div>
+      <div className="screen-max-width">
+        <div id="chip" className="flex-center w-full my-20">
+          <img src={chipImg} alt="chip" width={180} height={180} />
+        </div>
+      </div>
     </section>
   );
 };
