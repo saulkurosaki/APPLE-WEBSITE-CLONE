1a22dedcb1b0d5be6eb092ba8f0601b9a5995c11
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-30 20:19:13 -0600

    feat(Footer): implement the Footer component initial layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Highlights from "./components/Highlights";
 import Model from "./components/Model";
 import Features from "./components/Features";
 import HowItWorks from "./components/HowItWorks";
+import Footer from "./components/Footer";
 
 import * as Sentry from "@sentry/react";
 
@@ -16,6 +17,7 @@ const App = () => {
       <Model />
       <Features />
       <HowItWorks />
+      <Footer />
     </main>
   );
 };
